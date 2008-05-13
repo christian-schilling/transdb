7fddba178572b8012ac8256ef412bd0268125787
garcia.marc <garcia.marc@df2eced6-bd40-0410-90d2-0f9b653cd0f8>
2008-05-13 23:11:34 +0000

    allowing serialization, thanks Manuel Saelices avoiding bug when language names are translated using gettext_lazy, instead of ugettext_lazy Fixed #6: prepopulated fields will work for default language Fixed #8: removed unused and buggy code
    git-svn-id: http://transdb.googlecode.com/svn/trunk@9 df2eced6-bd40-0410-90d2-0f9b653cd0f8

--- fields.py
+++ fields.py
@@ -2,104 +2,108 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import get_language
 from django.utils.translation import ugettext as _
+from django.utils.encoding import force_unicode
 from django.newforms.fields import Field
 from django.newforms import ValidationError
 from widgets import TransCharWidget, TransTextWidget
 
 def get_default_language_name():
-	'''
-	Get language from default language specified by LANGUAGE_CODE in settings
-	Used in error messages
-	'''
-	lang_name = ''
-	for lang in settings.LANGUAGES:
-		if lang[0] == settings.LANGUAGE_CODE:
-			lang_name = lang[1]
-			break
-	return lang_name
+    '''
+    Get language from default language specified by LANGUAGE_CODE in settings
+    Used in error messages
+    '''
+    lang_name = ''
+    for lang in settings.LANGUAGES:
+        if lang[0] == settings.LANGUAGE_CODE:
+            lang_name = lang[1]
+            break
+    return force_unicode(lang_name)
 
 class TransDbValue(unicode):
-	'''
-	This class implements a unicode string, but with a hidden attribute raw_data.
-	When used as a string it returns the translation of the current language
-	raw_data attribute stores a dictionary with all translations
-	Also implements a method "get_in_language(language)" that returns the translation on any available language
-	'''
-	raw_data = {}
+    '''
+    This class implements a unicode string, but with a hidden attribute raw_data.
+    When used as a string it returns the translation of the current language
+    raw_data attribute stores a dictionary with all translations
+    Also implements a method "get_in_language(language)" that returns the translation on any available language
+    '''
+    raw_data = {}
 
-	def get_in_language(self, language):
-		if self.raw_data and self.raw_data.has_key(language):
-			return self.raw_data[language]
-		else:
-			return ''
+    def get_in_language(self, language):
+        if self.raw_data and self.raw_data.has_key(language):
+            return self.raw_data[language]
+        else:
+            return ''
 
 class TransFormField(Field):
-	'''
-	newforms field, used when ModelForm (or deprecated form_for_model/form_form_instance) is called
-	Also implements form validation in admin
-	'''
-	def clean(self, value):
-		if self.required and not value[settings.LANGUAGE_CODE]:
-			raise ValidationError, _("This field cannot be null for default language '%s'." % get_default_language_name())
-		else:
-			return value
+    '''
+    newforms field, used when ModelForm (or deprecated form_for_model/form_form_instance) is called
+    Also implements form validation in admin
+    '''
+    def clean(self, value):
+        if self.required and not value[settings.LANGUAGE_CODE]:
+            raise ValidationError, _("This field cannot be null for default language '%s'." % get_default_language_name())
+        else:
+            return value
 
 class TransField(models.Field):
-	'''
-	Model field to be subclassed
-	Used for storing a string in many languages at database (with python's dictionary format)
-	pickle module could be used, but wouldn't alow search on fields?
-	'''
-	def get_manipulator_field_objs(self):
-		return [FormTransField]
+    '''
+    Model field to be subclassed
+    Used for storing a string in many languages at database (with python's dictionary format)
+    pickle module could be used, but wouldn't alow search on fields?
+    '''
+    def get_internal_type(self):
+        return 'TextField'
 
-	def get_internal_type(self):
-		return 'TextField'
+    def to_python(self, value):
+        if isinstance(value, dict): # formfield method makes this function be called with value as a dict
+            python_value = value
+        else:
+            try:
+                python_value = eval(value)
+            except Exception:
+                python_value = None
+        if isinstance(python_value, dict) and (python_value.has_key(get_language()) or python_value.has_key(settings.LANGUAGE_CODE)):
+            if python_value.has_key(get_language()):
+                result = TransDbValue(python_value[get_language()])
+            elif python_value.has_key(settings.LANGUAGE_CODE):
+                result = TransDbValue(python_value[settings.LANGUAGE_CODE])
+            result.raw_data = python_value
+        else:
+            result = TransDbValue(value)
+            result.raw_data = {settings.LANGUAGE_CODE: value}
+        return result
+    
+    def get_db_prep_save(self, value):
+        return unicode(value.raw_data)
 
-	def to_python(self, value):
-		if isinstance(value, dict): # formfield method makes this function be called with value as a dict
-			python_value = value
-		else:
-			try:
-				python_value = eval(value)
-			except Exception:
-				python_value = None
-		if isinstance(python_value, dict) and python_value.has_key(settings.LANGUAGE_CODE):
-			if python_value.has_key(get_language()) and python_value[get_language()]:
-				result = TransDbValue(python_value[get_language()])
-			else:
-				result = TransDbValue(python_value[settings.LANGUAGE_CODE])
-			result.raw_data = python_value
-		else:
-			result = TransDbValue(value)
-			result.raw_data = {settings.LANGUAGE_CODE: value}
-		return result
-	
-	def get_db_prep_save(self, value):
-		return unicode(value.raw_data)
+    def formfield(self, **kwargs):
+        defaults = {'form_class': TransFormField}
+        defaults.update(kwargs)
+        return super(TransField, self).formfield(**defaults)
 
-	def formfield(self, **kwargs):
-		defaults = {'form_class': TransFormField}
-		defaults.update(kwargs)
-		return super(TransField, self).formfield(**defaults)
+    def flatten_data(self, follow, obj=None): 
+        '''
+        for serializing objects
+        '''
+        return {self.attname: self._get_val_from_obj(obj).raw_data} 
 
 class TransCharField(TransField):
-	'''
-	TransField used with CharField widget
-	'''
-	__metaclass__ = models.SubfieldBase
+    '''
+    TransField used with CharField widget
+    '''
+    __metaclass__ = models.SubfieldBase
 
-	def formfield(self, **kwargs):
-		kwargs['widget'] = TransCharWidget
-		return super(TransCharField, self).formfield(**kwargs)
+    def formfield(self, **kwargs):
+        kwargs['widget'] = TransCharWidget
+        return super(TransCharField, self).formfield(**kwargs)
 
 class TransTextField(TransField):
-	'''
-	TransField used with CharField widget
-	'''
-	__metaclass__ = models.SubfieldBase
+    '''
+    TransField used with CharField widget
+    '''
+    __metaclass__ = models.SubfieldBase
 
-	def formfield(self, **kwargs):
-		kwargs['widget'] = TransTextWidget
-		return super(TransTextField, self).formfield(**kwargs)
+    def formfield(self, **kwargs):
+        kwargs['widget'] = TransTextWidget
+        return super(TransTextField, self).formfield(**kwargs)
 
--- widgets.py
+++ widgets.py
@@ -1,47 +1,52 @@
 from django.newforms.widgets import Widget
 from django.conf import settings
 from django.utils.safestring import mark_safe
+from django.utils.encoding import force_unicode
 from django.newforms.util import flatatt
 
 class TransCharWidget(Widget):
-	'''
-	Widget that shows many labeled inputs
-	Can be subclassed to change the type of input to display (overwritting "get_input()" method) 
-	'''
-	def get_input(self, name, value, lang, attrs):
-		value_html = ''
-		if value:
-			value_html = ' value="%s"' % value
-		return '<input type="text" name="%s_%s"%s/>' % (name, lang, value_html)
-		
-	def render(self, name, value, attrs=None):
-		if value and hasattr(value, 'raw_data'):
-			value_dict = value.raw_data
-		else:
-			value_dict = {}
-		output = []
-		for lang_code, lang_name in settings.LANGUAGES:
-			value_for_lang = ''
-			if value_dict.has_key(lang_code):
-				value_for_lang = value_dict[lang_code]
-			output.append('<li style="list-style-type: none; float: left; margin-right: 1em;"><span style="display: block;">%s:</span>%s</li>' % (lang_name, self.get_input(name, value_for_lang, lang_code, attrs)))
-		return mark_safe(u'<ul id="id_%s">%s</ul>' % (name, u''.join(output)))
-	
-	def value_from_datadict(self, data, files, name):
-		value = {}
-		for lang_code, lang_name in settings.LANGUAGES:
-			value[lang_code] = data.get('%s_%s' % (name, lang_code))
-		return value
+    '''
+    Widget that shows many labeled inputs
+    Can be subclassed to change the type of input to display (overwritting "get_input()" method) 
+    '''
+    def get_input(self, name, value, lang, attrs, id=None):
+        value_html = (value and ' value="%s"' % value) or ''
+        id_html = (id and ' id="id_%s"' % name) or ''
+        return '<input type="text" name="%s_%s"%s%s/>' % (name, lang, value_html, id_html)
+
+    def render(self, name, value, attrs=None):
+        if value and hasattr(value, 'raw_data'):
+            value_dict = value.raw_data
+        else:
+            value_dict = {}
+        output = []
+        for lang_code, lang_name in settings.LANGUAGES:
+            value_for_lang = ''
+            if value_dict.has_key(lang_code):
+                value_for_lang = value_dict[lang_code]
+            if lang_code == settings.LANGUAGE_CODE:
+                input = self.get_input(name, value_for_lang, lang_code, attrs, True)
+            else:
+                input = self.get_input(name, value_for_lang, lang_code, attrs)
+            output.append('<li style="list-style-type: none; float: left; margin-right: 1em;"><span style="display: block;">%s:</span>%s</li>' % (force_unicode(lang_name), input))
+        return mark_safe(u'<ul>%s</ul>' % (u''.join(output)))
+    
+    def value_from_datadict(self, data, files, name):
+        value = {}
+        for lang_code, lang_name in settings.LANGUAGES:
+            value[lang_code] = data.get('%s_%s' % (name, lang_code))
+        return value
 
 class TransTextWidget(TransCharWidget):
-	'''
-	Subclasses TransCharField to use textarea insted of input
-	'''
-	def __init__(self, attrs=None):
-		self.attrs = {'cols': '40', 'rows': '10'}
-		if attrs:
-			self.attrs.update(attrs)
+    '''
+    Subclasses TransCharField to use textarea insted of input
+    '''
+    def __init__(self, attrs=None):
+        self.attrs = {'cols': '40', 'rows': '10'}
+        if attrs:
+            self.attrs.update(attrs)
 
-	def get_input(self, name, value, lang, attrs):
-		return '<textarea name="%s_%s"%s>%s</textarea>' % (name, lang, flatatt(self.attrs), value)
+    def get_input(self, name, value, lang, attrs, id=None):
+        id_html = (id and ' id="id_%s"' % name) or ''
+        return '<textarea name="%s_%s"%s%s>%s</textarea>' % (name, lang, flatatt(self.attrs), id_html, value)
 
