ec25e409cf7517f9138606cb347b08a462826da8
garcia.marc <garcia.marc@df2eced6-bd40-0410-90d2-0f9b653cd0f8>
2008-05-17 13:09:05 +0000

    Fixed #2. Installation script provided as well as few documentation files. Directory structure changed for achieving this.
    git-svn-id: http://transdb.googlecode.com/svn/trunk@11 df2eced6-bd40-0410-90d2-0f9b653cd0f8

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TransDb',
+        version='0.9',
+        description='TransDb allow translation of Django model fields',
+        author='Marc Garcia',
+        author_email='marc.garcia@accopensys.com',
+        url='http://code.google.com/p/transdb/',
+        packages=['transdb'],
+    )
