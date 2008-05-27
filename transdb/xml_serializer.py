75c1e0739e027e4413a233ad32be9e267393316d
msaelices <msaelices@df2eced6-bd40-0410-90d2-0f9b653cd0f8>
2008-05-27 09:18:31 +0000

    Add implementation of xml serializer for transdb. This should be included in settings like this:
    SERIALIZATION_MODULES = { 'xml' : 'transdb.xml_serializer' }
    
    This fixes #12.
    
    
    git-svn-id: http://transdb.googlecode.com/svn/trunk@16 df2eced6-bd40-0410-90d2-0f9b653cd0f8

---
+++ transdb/xml_serializer.py
@@ -0,0 +1,60 @@
+from django.core.serializers import base, xml_serializer
+from django.db import models
+
+from fields import TransField
+
+
+class Serializer(xml_serializer.Serializer):
+    pass
+
+
+class Deserializer(xml_serializer.Deserializer):
+    def _handle_object(self, node):
+        """
+        Convert an <object> node to a DeserializedObject.
+        """
+        # Look up the model using the model loading mechanism. If this fails,
+        # bail.
+        Model = self._get_model_from_node(node, "model")
+
+        # Start building a data dictionary from the object.  If the node is
+        # missing the pk attribute, bail.
+        pk = node.getAttribute("pk")
+        if not pk:
+            raise base.DeserializationError("<object> node is missing the 'pk' attribute")
+
+        data = {Model._meta.pk.attname : Model._meta.pk.to_python(pk)}
+
+        # Also start building a dict of m2m data (this is saved as
+        # {m2m_accessor_attribute : [list_of_related_objects]})
+        m2m_data = {}
+
+        # Deseralize each field.
+        for field_node in node.getElementsByTagName("field"):
+            # If the field is missing the name attribute, bail (are you
+            # sensing a pattern here?)
+            field_name = field_node.getAttribute("name")
+            if not field_name:
+                raise base.DeserializationError("<field> node is missing the 'name' attribute")
+
+            # Get the field from the Model. This will raise a
+            # FieldDoesNotExist if, well, the field doesn't exist, which will
+            # be propagated correctly.
+            field = Model._meta.get_field(field_name)
+
+            # As is usually the case, relation fields get the special treatment.
+            if field.rel and isinstance(field.rel, models.ManyToManyRel):
+                m2m_data[field.name] = self._handle_m2m_field_node(field_node, field)
+            elif field.rel and isinstance(field.rel, models.ManyToOneRel):
+                data[field.attname] = self._handle_fk_field_node(field_node, field)
+            else:
+                if field_node.getElementsByTagName('None'):
+                    value = None
+                elif isinstance(field, TransField):
+                    value = field.to_python(xml_serializer.getInnerText(field_node).strip()).raw_data
+                else:
+                    value = field.to_python(xml_serializer.getInnerText(field_node).strip())
+                data[field.name] = value
+
+        # Return a DeserializedObject so that the m2m data has a place to live.
+        return base.DeserializedObject(Model(**data), m2m_data)
